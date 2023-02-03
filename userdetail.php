6fef3fad2f6a3f4fd66f36b6c8884d3dc145ea7f
Peter-sunny <petersunny9778@gmail.com>
2023-02-03 19:48:33 +0530

    update

--- userdetail.php
+++ userdetail.php
@@ -18,7 +18,17 @@ $user_id = $_REQUEST['id'];
 $dataname = "SELECT *  FROM contacts WHERE id = '".$user_id."' ";
 $datanames = "";
 
+$result = $conn->query($dataname);
 
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+
+  $datanames .= '
+<input id="id_input" type="text" name=""value="'. $row["id"].'">
+  <p > '. $row["name"].' </p> <p>  '. $row["phone"]. ' </p><p>  '. $row["email"]. ' </p><p>  '. $row["address"]. ' </p>';
+
+  }
 } else {
   echo "0 results";
 }
