6fbb47f5d00514bd76d3eb42e1c3f2447bcc253a
Peter-sunny <petersunny9778@gmail.com>
2023-02-05 09:20:10 +0530

    display name from db

--- display_name.php
+++ display_name.php
@@ -3,7 +3,21 @@
 
             $dataname = "SELECT id,name,phone  FROM contacts";
             $result = $conn->query($dataname);
-           
+            $datas = '';
+
+
+
+                        // entered data printing
+
+            if ($result->num_rows > 0) {
+                        // output data of each row
+              while($row = $result->fetch_assoc()) {
+
+                $datas .= '<p  class="display_name" u_id="'. $row["id"]. '" > '. $row["name"]. '</p>';
+
+              }
+            } else {
+              echo "0 results";
             }
             echo $datas;
             ?>
